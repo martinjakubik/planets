e7a5aafa0b6d4d826b8e3f342a32b2f7117a4de1
martin jakubik <martin.jakubik@gmail.com>
2021-12-31 10:05:05 +0100

    added tests

--- test/lib.js
+++ test/lib.js
@@ -1,6 +1,104 @@
-import { calculatePosition } from '../lib.mjs';
+import { calculateGravity, calculatePosition } from '../lib.mjs';
 
 QUnit.module('lib');
+
+const BODY_1 = {
+    id: '1:1',
+    mass: 1,
+    initialPosition: {
+        x: 10,
+        y: 10
+    },
+    position: {
+        x: 10,
+        y: 10
+    },
+    force: 1,
+    angle: 0
+};
+
+const BODY_2 = {
+    id: '1:2',
+    mass: 1,
+    initialPosition: {
+        x: 10,
+        y: 10
+    },
+    position: {
+        x: 10,
+        y: 10
+    },
+    force: 1,
+    angle: 0
+};
+
+const BODY_3 = {
+    id: '1:3',
+    mass: 1,
+    initialPosition: {
+        x: 10,
+        y: 10
+    },
+    position: {
+        x: 10,
+        y: 10
+    },
+    force: 1,
+    angle: 0
+};
+
+const NEIGHBOUR_1 = {
+    id: '2:2',
+    mass: 1,
+    initialPosition: {
+        x: 20,
+        y: 10
+    },
+    position: {
+        x: 20,
+        y: 10
+    },
+    force: 1,
+    angle: Math.PI
+};
+
+QUnit.test('calculate flat angle', assert => {
+
+    const body = BODY_1;
+    const neighbour = NEIGHBOUR_1;
+
+    body.force = 1;
+    body.angle = 0;
+
+    neighbour.force = 1;
+    neighbour.angle = Math.PI;
+
+    const expected = 0;
+    const actualBody = calculateGravity(body, neighbour);
+    const actual =  actualBody.angle;
+
+    assert.equal(actual, expected);
+
+});
+
+QUnit.test('calculate vertical angle', assert => {
+
+    const body = BODY_1;
+    const neighbour = NEIGHBOUR_1;
+
+    body.force = 1;
+    body.angle = -1 * Math.PI / 2;
+
+    neighbour.force = 1;
+    neighbour.angle = Math.PI / 2;
+
+    const expected = -1 * Math.PI / 2;
+    const actualBody = calculateGravity(body, neighbour);
+    const actual =  actualBody.angle;
+
+    assert.equal(actual, expected);
+
+});
 
 QUnit.test('calculate position', assert => {
 
@@ -8,20 +106,20 @@ QUnit.test('calculate position', assert => {
         id: '100:100',
         mass: 1,
         initialPosition: {
-            x: 3,
-            y: 15
+            x: 1,
+            y: 18
         },
         position: {
             x: 3,
             y: 15
         },
-        force: 0.04,
-        angle: -Math.PI / 2
+        force: 0.005524271728019903,
+        angle: -Math.PI / 4
     };
 
     const expected = {
-        x: 4,
-        y: 15
+        x: 2,
+        y: 17
     };
     const actual = calculatePosition(body, 10);
 
