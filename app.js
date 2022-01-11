51ebdcc1b5d0605cbcbf51118a1c9e2468b5e5d4
martin jakubik <martin.jakubik@gmail.com>
2022-01-11 14:13:16 +0100

    saved to local storage

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { makeSpaceGrid, makeSpaceTimeButtonBar, aSpaceTime } from './lib.mjs';
-import { makeDataButtonBar } from './data.mjs';
+import { makeDataButtonBar, makeLoadBar } from './data.mjs';
 
 makeSpaceGrid(80);
 makeSpaceTimeButtonBar();
 makeDataButtonBar(aSpaceTime);
+makeLoadBar();
--- data.mjs
+++ data.mjs
@@ -1,6 +1,6 @@
 import { createDiv, createButton } from './lib/js/learnhypertext.mjs';
 
-const handleSaveButtonClick = function () {
+const handleDownloadButtonClick = function () {
     const sContent = JSON.stringify(aSpaceTime);
     const a = document.createElement('a');
     a.href = `data:application/json,${sContent}`;
@@ -11,8 +11,21 @@ const handleSaveButtonClick = function () {
     document.body.removeChild(a);
 };
 
-const handleLoadButtonClick = function () {
+const handleSaveButtonClick = function () {
+    const sContent = JSON.stringify(aSpaceTime);
+    const oLocalStorage = window.localStorage;
+    const sNowLabel = getNowLabel();
+    const sKey = `spacetime-${sNowLabel}`;
+    oLocalStorage.setItem(sKey, sContent);
+    handleStorageChange();
+};
 
+const handleLoadFileInputChange = function () {
+};
+
+const makeDownloadSpaceTimeButton = function (parentBox) {
+    const oButton = createButton('downloadSpaceTimeButton', 'Download', parentBox);
+    oButton.onclick = handleDownloadButtonClick;
 };
 
 const makeSaveSpaceTimeButton = function (parentBox) {
@@ -20,20 +33,23 @@ const makeSaveSpaceTimeButton = function (parentBox) {
     oButton.onclick = handleSaveButtonClick;
 };
 
-const makeLoadSpaceTimeButton = function (parentBox) {
-    const oButton = createButton('loadSpaceTimeButton', 'Load', parentBox);
-    oButton.onclick = handleLoadButtonClick;
+const makeUploadSpaceTimeButton = function (parentBox) {
+    const sAccept = 'json';
+    loadSpaceTimeButton = createFileInput('loadSpaceTimeButton', 'Upload', parentBox, sAccept);
+    loadSpaceTimeButton.addEventListener('change', handleLoadFileInputChange);
 };
 
 let aSpaceTime;
+let loadSpaceTimeButton;
 
 const makeDataButtonBar = function (spaceTime) {
     const buttonBar = createDiv('dataButtonBar');
 
     aSpaceTime = spaceTime;
 
+    makeDownloadSpaceTimeButton(buttonBar);
     makeSaveSpaceTimeButton(buttonBar);
-    // makeLoadSpaceTimeButton(buttonBar);
+    makeUploadSpaceTimeButton(buttonBar);
 };
 
 const getNowLabel = function () {
@@ -42,4 +58,62 @@ const getNowLabel = function () {
     return sNowLabel;
 };
 
-export { makeDataButtonBar };
+const createFileInput = function (sId, sLabel, oParent, sAccept) {
+
+    if (!oParent) {
+        oParent = document.body;
+    }
+
+    const oInput = document.createElement('input');
+    oInput.type = 'file';
+    oInput.id = sId;
+    if (sAccept && sAccept.length > 0) {
+        oInput.accept = sAccept;
+    }
+
+    const oLabel = document.createElement('label');
+    oLabel.for = sId;
+    oLabel.innerText = sLabel;
+
+    oParent.appendChild(oLabel);
+    oParent.appendChild(oInput);
+
+    return oInput;
+
+};
+
+const updateStorageView = function (storageArea) {
+    let oStorageView = document.getElementById('storageView');
+    if(!oStorageView) {
+        oStorageView = document.createElement('ul');
+        oStorageView.id = 'storageView';
+        document.body.appendChild(oStorageView);
+    }
+    const aChildren = oStorageView.childNodes;
+    for (let j = aChildren.length - 1; j >= 0; j--) {
+        const oChild = aChildren[j];
+        oStorageView.removeChild(oChild);
+    }
+    for (let i = 0; i < storageArea.length; i++) {
+        const sKey = storageArea.key(i);
+        const oChild = document.createElement('li');
+        oChild.innerText =sKey;
+        oStorageView.appendChild(oChild);
+    }
+};
+
+const handleStorageChange = function (storageEvent) {
+    let oStorageArea;
+    if (storageEvent) {
+        oStorageArea  = storageEvent.storage;
+    } else {
+        oStorageArea = window.localStorage;
+    }
+    updateStorageView(oStorageArea);
+};
+
+const makeLoadBar = function () {
+    window.addEventListener('storage', handleStorageChange);
+};
+
+export { makeDataButtonBar, makeLoadBar };
