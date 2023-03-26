702ccc5310a341605382a7b9f2fd1b5979ec4fed
martin jakubik <martin.jakubik@sap.com>
2023-03-26 21:04:19 +0200

    moved chirp to web root

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ import { SpaceTimeController } from './spacetimecontroller.mjs';
 const oAppConfiguration = {
     gridSize: 0,
     maxSpaceTimeSize: 10 ** 6,
-    hoverSound: 'chirp.mp3',
+    hoverSound: 'https://www.supertitle.org/chirp.mp3',
     volumeIcon: {
         on: 'https://www.supertitle.org/volume-on.png',
         off: 'https://www.supertitle.org/volume-off.png'
